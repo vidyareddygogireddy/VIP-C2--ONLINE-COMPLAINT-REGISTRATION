01537ca5a1c3e7de5b25a133a9d91fb87aec01e7
vidyareddygogireddy <vidyareddygogireddy@gmail.com>
2026-06-27 23:45:58 +0530

    fix(db): restore missing try statement in connectDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  try {
     const dbURI = (process.env.MONGO_URI && !process.env.MONGO_URI.includes('localhost')) 
       ? process.env.MONGO_URI 
       : (process.env.MONGO_URL || 'mongodb://localhost:27017/complaint-reg');
